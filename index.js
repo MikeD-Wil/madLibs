10d453a6019aa73aee4f7bfaeb1056262954b7b0
Mike <mikedon.wil@gmail.com>
2021-10-20 09:43:14 -0400

    Kobe

---
+++ index.js
@@ -0,0 +1,30 @@
+function createParagraph(){
+    var wordOne = document.getElementById;
+    var wordTwo = documnet.getElementById;
+    var wordThree = documnet.getElementById;
+    var wordFour= documnet.getElementById;
+    var wordFive = documnet.getElementById;
+    var wordSix = documnet.getElementById;
+    var wordSeven = documnet.getElementById;
+    var wordEight = documnet.getElementById;
+}
+
+
+{var paragraph =<p> ("Name") was a very curious (animal). He was always looking for adventure.
+        One day when he was at (a place) , he found a very mysterious note. It said: Go to (another place) tomorrow and you will get a big surprise!
+        (Name) was so excited that he told all his friends about it. No one seemed to believe him, even when he showed them the note.
+        The next morning, (Name) quickly got dressed and went to Dallas. When he got there it was very quiet.
+        He looked around and, all of a sudden, his friends jumped out of their hiding places and yelled, "Surprise, Happy Birthday!"
+        (Name) was so surprised he started to (an action)!
+
+        ~The End~
+     </div>
+
+
+    </p>
+
+    document.getElementById('answer').innerHTML = paragraph;
+}
+
+var word1 = document.getElementById('word1' +).value
+
